1971330fa104bf85dd8d525ee861bc4896409f69
Silas Barta <sbarta@gmail.com>
2013-08-24 22:37:09 -0700

    test coverage for board reset

--- functional_test.py
+++ functional_test.py
@@ -6,18 +6,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 DEBUG_BUTTON_SELECTOR = "button.debug"
 
-class PythonOrgSearch(unittest.TestCase):
+class UserInputtingNumbers(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.css = self.driver.find_element_by_css_selector
+        self.driver.get("http://localhost:8000/index.html")
 
-    def test_set_work_area_vals(self):
+    def test_indicate_error_vals(self):
         expected = u'2'
         driver = self.driver
-        driver.get("http://localhost:8000/index.html")
         self.css(DEBUG_BUTTON_SELECTOR).click()
-        WebDriverWait(driver, 500)
         workcell = self.css(".workarea td[posy='1'][posx='0']")
         choicecell = self.css(".numchoices td[tabindex='1']")
         # click the buttons to set tens place on 1st row to 2
@@ -28,6 +27,28 @@ class PythonOrgSearch(unittest.TestCase):
         # check that it has error
         work_classes = workcell.get_attribute("class")
         self.assertIn("error", work_classes)
+        # then correct error by placing 1 there
+        choicecell2 = self.css(".numchoices td[tabindex='0']")
+        choicecell2.click()
+        self.assertEqual(u'1', workcell.text)
+        work_classes = workcell.get_attribute("class")
+        self.assertNotIn("error", work_classes)
+
+    def test_input_vals_after_restart(self):
+        expected = u'8'
+        driver = self.driver
+        workcell = self.css(".workarea td[posy='2'][posx='1']")
+        choicecell = self.css(".numchoices td[tabindex='7']")
+        workcell.click()
+        choicecell.click()
+        # clear board and enter text again
+        self.css(DEBUG_BUTTON_SELECTOR).click()
+        workcell = self.css(".workarea td[posy='2'][posx='1']")
+        choicecell = self.css(".numchoices td[tabindex='7']")
+        workcell.click()
+        choicecell.click()
+        actual = workcell.text
+        self.assertEqual(expected, actual)
 
     def tearDown(self):
         self.driver.close()
